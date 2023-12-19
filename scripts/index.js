d58c8dcd52d610cbf2f02a9b501a41cc4a26e076
bouderton <rmkhazal@gmail.com>
2023-12-18 20:19:13 -0800

    Data classifications

--- scripts/index.js
+++ scripts/index.js
@@ -3,3 +3,8 @@ import Calculator from '../components/Calculator.js';
 const numberBtns = document.querySelectorAll('.calc__grid-btn');
 const settingBtns= document.querySelectorAll('.calc__span-two');
 
+const operations = document.querySelectorAll('[data-operation]');
+const numbers = document.querySelectorAll('[data-number]');
+
+console.log(numbers);
+console.log(operations);
