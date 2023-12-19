11ec3e139fe04ce543f4a994112f4497ec2df6f7
bouderton <rmkhazal@gmail.com>
2023-12-18 18:06:00 -0800

    OOP setup

---
+++ components/Calculator.js
@@ -0,0 +1,14 @@
+export default class Calculator  {
+    constructor(){
+
+    }
+
+    clear(){}
+
+    delete(){}
+
+    appendNumber(){}
+
+    choseOperation(){}
+
+}
--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,5 @@
+import Calculator from '../components/Calculator.js';
+
 const numberBtns = document.querySelectorAll('.calc__grid-btn');
 const settingBtns= document.querySelectorAll('.calc__span-two');
 
