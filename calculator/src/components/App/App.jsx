5da13a4f6098dd87bc99fe197f8703423bde924a
bouderton <rmkhazal@gmail.com>
2024-03-25 10:24:46 -0700

    button

--- calculator/src/components/App/App.jsx
+++ calculator/src/components/App/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from "../../assets/react.svg";
 import viteLogo from "../../../public/vite.svg";
 import Calculator from "../Calculator/Calculator";
 import Screen from "../Screen/Screen";
+import ButtonGrid from "../ButtonGrid/ButtonGrid";
+import Button from "../Button/Button";
 import "./App.css";
 
 function App() {
@@ -12,9 +14,17 @@ function App() {
     <>
       <div>
         <h1 className="calc__header">SHITTY CALCULATOR INCOMING</h1>
-
         <Calculator>
-          <Screen />
+          <Screen value={0} />
+          <ButtonGrid>
+            <Button
+              value={0}
+              className=""
+              onClick={() => {
+                console.log("Click");
+              }}
+            />
+          </ButtonGrid>
         </Calculator>
       </div>
     </>
